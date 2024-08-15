00ecf0da5d341a72bb65b81b5abdc9d464029ed6
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-08-15 16:04:02 +0400

    feat: optimize texts

--- src/helpers/generateFinalText.ts
+++ src/helpers/generateFinalText.ts
@@ -10,14 +10,15 @@ export default function generateFinalText(
 
   const intro = generateIntro(selectedPlace, selectedTournament);
   const categoryPlayers = generateCategoryPlayers(players);
-  const bestGirl = generatePrizer(girl, "Լավագույն աղջիկ");
-  const thirdPlace = generatePrizer(third, "3-րդ տեղ գրավեց");
-  const secondPlace = generatePrizer(second, "2-րդ տեղ գրավեց");
-  const firstPlace = generatePrizer(first, "Մրցաշարի հաղթող");
+  const prizersIntro = "Մրցանակային տեղ գրաված մասնակիցներն են՝";
+  const bestGirl = generatePrizer(girl, "Լավագույն աղջիկ` ");
+  const thirdPlace = generatePrizer(third, "3-րդ տեղ` ");
+  const secondPlace = generatePrizer(second, "2-րդ տեղ` ");
+  const firstPlace = generatePrizer(first, "Մրցաշարի հաղթող` ");
   const epilog =
     "Շնորհավորում ենք մրցանակակիրներին և մաղթում նորանոր հաջողություններ";
 
-  return `${intro}\n${categoryPlayers}\n${bestGirl}\n${thirdPlace}\n${secondPlace}\n${firstPlace}\n${epilog}`;
+  return `${intro}\n${categoryPlayers}\n${prizersIntro}\n${bestGirl}\n${thirdPlace}\n${secondPlace}\n${firstPlace}\n${epilog}`;
 }
 
 function generateIntro(selectedPlace: string, selectedTournament: string) {
@@ -44,5 +45,5 @@ function generatePrizer({ player, trainer }: IPlayer, prize: string = "") {
     text += ` (մարզիչ՝ ${trainer})`;
   }
 
-  return `${text}\n`;
+  return `${text}:\n`;
 }
