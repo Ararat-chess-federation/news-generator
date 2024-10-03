2bb3b128d0b7d6a894537e28a0a03aaa8c4796e8
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-10-03 11:15:36 +0400

    feat: refactor code

--- src/helpers/generateAdText.ts
+++ src/helpers/generateAdText.ts
@@ -9,6 +9,34 @@ export default function generateAdText(
   phoneNumber: string
 ) {
   const days = selectedDays.slice(0, selectedDays.indexOf("0"));
+  const intro = generateIntro(
+    selectedPlace,
+    selectedTournament,
+    selectedDays,
+    selectedMonths,
+    days
+  );
+
+  const schedule = generateSchedule(
+    days,
+    selectedDays,
+    selectedMonths,
+    selectedTimes
+  );
+
+  const deadline = generateDeadLine(deadLineMonth, deadLineDay);
+  const number = generateNumber(phoneNumber);
+  
+  return `${intro}\n ${schedule}\n ${deadline}\n ${number}`;
+}
+
+function generateIntro(
+  selectedPlace: string,
+  selectedTournament: string,
+  selectedDays: string[],
+  selectedMonths: string[],
+  days: string[]
+) {
   const startDate = `${selectedMonths[0]}ի ${selectedDays[0]}-ից`;
   const endDate = `${selectedMonths[days.length - 1]}ի ${
     selectedDays[days.length - 1]
@@ -18,8 +46,16 @@ export default function generateAdText(
     startDate.charAt(0).toUpperCase() + startDate.slice(1)
   } ${endDate}`;
 
-  const intro = `${term} ${selectedPlace} տեղի կունենա ${selectedTournament} որակավորման մրցաշար հետևյալ ժամանակացույցով՝\n`;
-  const schedule = days.reduce((acc, val, idx) => {
+  return `${term} ${selectedPlace} տեղի կունենա ${selectedTournament} որակավորման մրցաշար հետևյալ ժամանակացույցով՝\n`;
+}
+
+function generateSchedule(
+  days: string[],
+  selectedDays: string[],
+  selectedMonths: string[],
+  selectedTimes: string[]
+) {
+  return days.reduce((acc, val, idx) => {
     return (
       acc +
       `- Տուր ${idx + 1}՝ ${selectedMonths[idx]}ի ${selectedDays[idx]} ժամը ${
@@ -27,7 +63,12 @@ export default function generateAdText(
       }\n`
     );
   }, "");
-  const deadline = `Գրանցման վերջնաժամկետն է՝ ${deadLineMonth} ${deadLineDay} ժամը 18:00։`;
-  const number = `Գրանցվելու համար զանգահարել ${phoneNumber} հեռախոսահամարով`;
-  return intro + schedule + deadline + number;
+}
+
+function generateDeadLine(deadLineMonth: string, deadLineDay: string) {
+  return `Գրանցման վերջնաժամկետն է՝ ${deadLineMonth} ${deadLineDay} ժամը 18:00։`;
+}
+
+function generateNumber(phoneNumber: string) {
+  return `Գրանցվելու համար զանգահարել ${phoneNumber} հեռախոսահամարով`;
 }
