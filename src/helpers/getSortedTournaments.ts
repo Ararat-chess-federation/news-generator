d0ce062fcad6258a4c75cc8b12a8c93ad53f7fc5
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2026-05-31 22:27:49 +0400

    fix: correct multiple tournaments if >2 tournament

--- src/helpers/getSortedTournaments.ts
+++ src/helpers/getSortedTournaments.ts
@@ -6,20 +6,28 @@ export default function getSortedTournaments(finishedTournaments: ITournament[])
     let combinedTournaments: ITournament[] = [];
     let regularTournaments: ITournament[] = [];
 
-    if (combinedIndex !== -1) {
-        const combinedTournament = finishedTournaments[combinedIndex];
-        const { place } = parseTournamentTitle(combinedTournament.title);
-        const pairIdx = finishedTournaments.findIndex(
-            (t, idx) => idx !== combinedIndex && t.title.includes(place)
-        );
-
-        combinedTournaments = [combinedTournament, finishedTournaments[pairIdx]];
-        regularTournaments = finishedTournaments.filter(
-            (_, idx) => idx !== combinedIndex && idx !== pairIdx
-        );
-    } else {
+    if (combinedIndex > -1) {
         regularTournaments = finishedTournaments;
+
+        return { combinedTournaments, regularTournaments }
     }
-    
+
+    const combinedTournament = finishedTournaments[combinedIndex];
+    const { place } = parseTournamentTitle(combinedTournament.title);
+    const pairIdx = finishedTournaments.findIndex(
+        (t, idx) => idx !== combinedIndex && t.title.includes(place) && t.title[0] === combinedTournament.title[0]
+    );
+
+    if (pairIdx === -1) {
+        regularTournaments = finishedTournaments;
+
+        return { combinedTournaments, regularTournaments }
+    }
+
+    combinedTournaments = [combinedTournament, finishedTournaments[pairIdx]];
+    regularTournaments = finishedTournaments.filter(
+        (_, idx) => idx !== combinedIndex && idx !== pairIdx
+    );
+
     return { combinedTournaments, regularTournaments }
 }
